a33e18199b2d611ac2f8bdd4f99976a49d726b00
Евгений <evgeniy.rekw@gmail.com>
2025-05-26 16:44:13 +0500

    Создание новой формы

---
+++ main/forms.py
@@ -0,0 +1,24 @@
+# forms.py
+from django import forms
+from .models import Comment
+
+class CommentForm(forms.ModelForm):
+    author_name = forms.CharField(
+        label='Ваше имя',
+        required=False,  # Необязательное поле
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Иван Иванов (необязательно)'
+        })
+    )
+    
+    class Meta:
+        model = Comment
+        fields = ['author_name', 'text']
+        widgets = {
+            'text': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 3,
+                'placeholder': 'Оставьте ваш комментарий...'
+            })
+        }
+        
